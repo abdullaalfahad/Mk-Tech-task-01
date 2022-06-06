8029e5273d7fcb05d4998da701d5b3ecf9663a61
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-06-06 14:19:02 +0600

    banner added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,32 +12,32 @@ const Navbar = () => {
     </>
     return (
         <nav>
-            <div class="navbar">
-                <div class="navbar-start">
-                    <a class="btn btn-ghost normal-case text-xl">LOGO</a>
+            <div className="navbar">
+                <div className="navbar-start">
+                    <a className="btn btn-ghost normal-case text-xl">LOGO</a>
                 </div>
-                <div class="navbar-end">
-                    <a class="mr-10"><span className='text-success'>BN</span> / EN</a>
+                <div className="navbar-end">
+                    <a className="mr-10"><span className='text-success'>BN</span> / EN</a>
                 </div>
             </div>
 
-            <div class="navbar">
-                <div class="navbar-start">
-                    <div class="dropdown">
-                        <label tabindex="0" class="lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div className="navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex="0" className="lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-primary">
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-primary">
                             {menuItems}
                         </ul>
                     </div>
-                    <div class="navbar-center hidden lg:flex">
-                        <ul class="menu menu-horizontal p-0">
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal p-0">
                             {menuItems}
                         </ul>
                     </div>
                 </div>
-                <div class="navbar-end">
+                <div className="navbar-end">
                     <p className='bg-white px-6 py-2 rounded-full'><button className='mr-3 text-accent'>LOGIN</button><button className='btn btn-secondary text-white rounded-full '>SIGN UP</button></p>
                 </div>
             </div>
